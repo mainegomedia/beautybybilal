26d8c62f25556ec509bce95306e38ab948f3ba2c
mainegomedia <mainegomedia@gmail.com>
2022-04-14 13:01:00 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+if ('serviceWorker' in navigator) {
+	
+	navigator.serviceWorker
+		.register('./service-worker.js', {scope:'./'})
+		.then(function(registration) {
+			console.log("Service Worker Registered", registration);
+	})
+	.catch(function(err) {
+		console.log("Service Worker Failed to Register", err);
+	});
+}
+
